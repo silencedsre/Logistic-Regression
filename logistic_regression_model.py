6ff7547d7f846bd6e2a907124bcdd0ba014fc22a
shree <jamakatel.shree.krishna@gmail.com>
2019-11-16 18:52:17 +0545

    initial commit for logistic regression

---
+++ logistic_regression_model.py
@@ -0,0 +1,13 @@
+class LogisticRegression:
+    def __init__(self):
+        pass
+
+
+    def propagate(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def model(self):
+        pass
